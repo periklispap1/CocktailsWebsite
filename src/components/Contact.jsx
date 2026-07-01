b8411d80b422e543a3dd0243ea6b5594fef76803
Periklis Papakostas <periklispap1@gmail.com>
2026-07-01 20:32:36 +0300

    minor contact section change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
             })
             .to('#f-left-leaf', {
                 y: -50, duration: 1, ease: 'power1.inOut',
-            },'<')
+            })
     })
 
     return (
